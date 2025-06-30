505c6ef62ac9dd166a1bd661d08af0ae5864cf76
Ryan Lee <ryan.j.lee09@gmail.com>
2025-06-29 22:27:55 -0700

    fix for audio

--- server/serialBridge.js
+++ server/serialBridge.js
@@ -1,12 +1,15 @@
-const SerialPort = require('serialport');
-const Readline = require('@serialport/parser-readline');
+const { SerialPort } = require('serialport'); // ✅ destructure the class
+const { ReadlineParser } = require('@serialport/parser-readline');
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 
 // Serial port setup
-const port = new SerialPort('COM5', { baudRate: 115200 });
-const parser = port.pipe(new Readline({ delimiter: '\n' }));
+const port = new SerialPort({
+  path: 'COM5',
+  baudRate: 115200,
+});
+const parser = port.pipe(new ReadlineParser({ delimiter: '\n' }));
 
 // Web server + Socket.io
 const app = express();
