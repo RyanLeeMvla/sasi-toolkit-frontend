fe1a0d5aed4f0d3c395d9f5bed0b87f8078352b8
Ryan Lee <ryan.j.lee09@gmail.com>
2025-07-01 12:54:35 -0700

    Filling in Supabase info

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,8 @@
-// frontend/src/supabaseClient.js
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabase = createClient(
+  process.env.REACT_APP_SUPABASE_URL,
+  process.env.REACT_APP_SUPABASE_ANON_KEY
+)
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export default supabase
