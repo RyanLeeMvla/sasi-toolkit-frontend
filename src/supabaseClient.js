c0422cd876fc3e88fecb51aadf37eb1c33e3f0bb
Ryan Lee <ryan.j.lee09@gmail.com>
2025-07-01 15:29:20 -0700

    changed supa.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,7 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-const supabase = createClient(
-  process.env.REACT_APP_SUPABASE_URL,
-  process.env.REACT_APP_SUPABASE_ANON_KEY
-)
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
-export default supabase
+const supabase = createClient(supabaseUrl, supabaseKey);
+export default supabase;
